63fdcd5ddc02321f6ec7695d642958381362997a
Nachoweb <nachoelg@gmail.com>
2013-02-25 14:25:42 +0100

    autorización

--- segurosReport.js
+++ segurosReport.js
@@ -6,6 +6,7 @@ app.use(function(err, req, res, next){
     console.error(err.stack);
     res.send(500, 'Something broke!');
 });
+app.use(express.basicAuth('miriam', 'seguros'));
 app.use(express.compress());
 app.use(express.bodyParser());
 app.use(express.static(__dirname + '/public'));
